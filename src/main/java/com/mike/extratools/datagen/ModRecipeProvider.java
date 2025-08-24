5ea66dc38a1791f910359e4ef5787f046dfeb73a
mike <mikezhou05@126.com>
2025-08-25 01:18:51 +0800

    更新了DataGen

--- src/main/java/com/mike/extratools/datagen/ModRecipeProvider.java
+++ src/main/java/com/mike/extratools/datagen/ModRecipeProvider.java
@@ -1,13 +1,26 @@
 package com.mike.extratools.datagen;
 
+import com.mike.extratools.ExtraToolsMod;
 import com.mike.extratools.blocks.ModBlocks;
 import com.mike.extratools.item.ModItems;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.chunk.storage.ChunkSerializer;
+import net.neoforged.neoforge.common.conditions.*;
+import net.neoforged.neoforge.common.crafting.ConditionalRecipeOutput;
+import org.spongepowered.include.com.google.gson.JsonObject;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ModRecipeProvider extends RecipeProvider {
     public ModRecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
@@ -15,37 +28,34 @@ public class ModRecipeProvider extends RecipeProvider {
     }
 
 
-
     @Override
-    protected void buildRecipes(RecipeOutput recipeOutput){
+    protected void buildRecipes(RecipeOutput recipeOutput) {
 
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.SILVER_BLOCK.get())
                 .pattern("BBB")
                 .pattern("BBB")
                 .pattern("BBB")
                 .define('B', ModItems.SILVER_INGOT.get())
-                .unlockedBy("has_silver_ingot",has(ModItems.SILVER_INGOT)).save(recipeOutput);
+                .unlockedBy("has_silver_ingot", has(ModItems.SILVER_INGOT)).save(recipeOutput);
 
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.SILVER_INGOT.get(),9)
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.SILVER_INGOT.get(), 9)
                 .requires(ModBlocks.SILVER_BLOCK.get())
-                .unlockedBy("has_silver_block",has(ModBlocks.SILVER_BLOCK)).save(recipeOutput);
+                .unlockedBy("has_silver_block", has(ModBlocks.SILVER_BLOCK)).save(recipeOutput);
 
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC,ModItems.COPPER_SWORD.get())
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.COPPER_SWORD.get())
                 .pattern(" B ")
                 .pattern(" B ")
                 .pattern(" A ")
-                .define('B',Items.COPPER_INGOT)
-                .define('A',Items.STICK)
-                .unlockedBy("has_copper_ingot",has(Items.COPPER_INGOT)).save(recipeOutput);
-        ShapedRecipeBuilder.shaped(RecipeCategory.MISC,ModItems.COPPER_PICKAXE.get())
+                .define('B', Items.COPPER_INGOT)
+                .define('A', Items.STICK)
+                .unlockedBy("has_copper_ingot", has(Items.COPPER_INGOT)).save(recipeOutput);
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.COPPER_PICKAXE.get())
                 .pattern("BBB")
                 .pattern(" A ")
                 .pattern(" A ")
-                .define('B',Items.COPPER_INGOT)
-                .define('A',Items.STICK)
-                .unlockedBy("has_copper_ingot",has(Items.COPPER_INGOT)).save(recipeOutput);
-
-
+                .define('B', Items.COPPER_INGOT)
+                .define('A', Items.STICK)
+                .unlockedBy("has_copper_ingot", has(Items.COPPER_INGOT)).save(recipeOutput);
 
     }
 }
